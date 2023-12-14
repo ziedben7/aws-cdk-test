8bea23c4d0e435b8892a485c63054f59fcde8ee8
Zied Bennani <56529403+ziedben7@users.noreply.github.com>
2023-12-14 15:34:02 +0100

    create vpc with 3 public and 3 private subnets with routs configs

--- cdk-test/lib/cdk-test-stack.ts
+++ cdk-test/lib/cdk-test-stack.ts
@@ -5,12 +5,42 @@ import { Construct } from 'constructs';
 export class CdkTestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkTestQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    
+    const vpc = new ec2.Vpc(this, 'cdk-test-vpc', {
+      cidr: '10.0.0.0/16',
+      natGateways: 1,
+      subnetConfiguration: [
+        {
+          name: 'private-one',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+          cidrMask: 24,
+        },
+        {
+          name: 'private-two',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+          cidrMask: 24,
+        },
+        {
+          name: 'private-three',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+          cidrMask: 24,
+        },
+        {
+          name: 'public-one',
+          subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 24,
+        },
+        {
+          name: 'public-two',
+          subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 24,
+        },
+        {
+          name: 'public-three',
+          subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 24,
+        },
+      ],
+    });
   }
 }
