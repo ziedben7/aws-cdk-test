ed5e5236a43e15c16c0b8ccef98abe075f6e654e
Zied Bennani <zied.bennanig@gmail.com>
2023-12-16 11:16:16 +0100

    default region

--- bin/aws-cdk-test.ts
+++ bin/aws-cdk-test.ts
@@ -7,7 +7,7 @@ const app = new cdk.App();
 new AwsCdkTestStack(app, 'AwsCdkTestStack', {
   stackName: 'cdk-test-stack',
   env: {
-    region: process.env.CDK_DEFAULT_REGION,
+    region: process.env.CDK_DEFAULT_REGION || 'us-east-2',
     account: process.env.CDK_DEFAULT_ACCOUNT,
   },
 
